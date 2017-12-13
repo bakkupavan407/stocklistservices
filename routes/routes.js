81f1773e660678792dfa9d0be1cd27fa113d7a7b
Pavan Kumar Bakku <Pavan.Bakku@macys.com>
2017-12-13 17:28:47 +0530

    allowed header key

--- routes/routes.js
+++ routes/routes.js
@@ -16,7 +16,8 @@ module.exports = {
         // to allow the CORS
         app.use(function(req, res, next) {
             res.header("Access-Control-Allow-Origin", "*");
-            res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+            res.header('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+            res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-access-token");
             next();
         });
 
